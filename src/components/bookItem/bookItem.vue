706bcbc69f3d6b30af3b05ed2a40b17db809e466
huangmengxuan <huangmengxuan@bytedance.com>
2019-03-22 19:40:24 +0800

    feat: 修改bookList样式
    Change-Id: I9fb1c7db1f59a1eb7114e876ffc52970d8b98580

--- src/components/bookItem/bookItem.vue
+++ src/components/bookItem/bookItem.vue
@@ -1,21 +1,23 @@
 <template>
   <van-card
     class="item-card van-hairline--bottom"
-    title="go语言实战"
-    :thumb="data.pictureUrlList[2]"
+    :thumb="data.coverUrl"
     tag="全新"
     desc="上学期上课的教材，九成新，现在随缘出售，想要的先私我。"
     :origin-price="data.originalPrice"
-    :price="data.sellPrice"
   >
     <div slot="tags">
       <van-tag round plain>
-        线下交易
+        {{ commodityType }}
       </van-tag>
       <van-tag round plain>
-        全新
+        {{ data.category }}
       </van-tag>
     </div>
+    <div slot="title" class="my-title">
+      {{ data.bookName }}
+    </div>
+    <div slot="price" class="my-price">￥{{ data.sellPrice }}</div>
   </van-card>
 </template>
 
@@ -27,6 +29,19 @@ export default {
       type: String,
       default: ''
     }
+  },
+  computed: {
+    commodityType() {
+      if (this.data.commodityType) {
+        if (this.data.commodityType === 'SELL') {
+          return '出售';
+        } else {
+          return '求购';
+        }
+      } else {
+        return '';
+      }
+    }
   }
 };
 </script>
@@ -36,21 +51,14 @@ export default {
   background-color white
   padding 12px
   margin-top 0
-.van-card__title
-  font-size 16px
-  -webkit-line-clamp 1
-  overflow-y visible
-.van-card__price
-  font-size 20px
-.van-card__thumb
-  width 100px
-  height 100px
-.van-card__tags
-  color: red
-.van-card__desc
-  font-size 12px
   padding 4px 0
 .van-tag
-  margin 4px 0
   margin-right 4px
+.my-title
+  overflow hidden
+  text-overflow ellipsis
+  white-space nowrap
+  font-size 16px
+.my-price
+  font-size 20px
 </style>
